8e408ad8cacaa3d606f9c02a461129824e4d4780
marrwan <abdulbasitdamilola6@gmail.com>
2023-03-21 10:46:13 +0100

    Add team image to Tree componentThe commit adds an image of the team to the Tree component and updates the Tree function to receive an idn prop

--- Downloads/tron-main/src/components/Team/Tree.js
+++ Downloads/tron-main/src/components/Team/Tree.js
@@ -1,9 +1,10 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import Sidebar from "../Sidebar";
+import Team from '../../img/team.jpg'
 
 
-const Tree = () => {
+const Tree = ({idn}) => {
     return (
         <div className="content">
     <Sidebar />
@@ -12,16 +13,16 @@ const Tree = () => {
                 <div className="container-fluid pt-4 px-4">
                     <div className="row g-4">
                    {/* Image here */}
+
+                                <img src={Team} alt="..." className=" mb-3" />
                      <div className="col-12 col-sm-6 col-md-4 col-lg-3">
                         <div className="card bg-secondary border-0">
                             <div className="card-body text-center">
-                                <img src="https://i.imgur.com/1Q9ZQ2r.png" alt="..." className="img-fluid rounded-circle mb-3" />
                                 <h5 className="card-title mb-0">User Name</h5>
-                                <div className="card-text text-black-50">User Id</div>
+                                <div className="card-text text-black-50">{idn}</div>
                                 </div>
                                 </div>
                                 </div>
-                                
                     </div>
                 </div>
               <div className='container-fluid pt-4 px-4'>
